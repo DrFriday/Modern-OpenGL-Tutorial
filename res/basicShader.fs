c5bb2bb2d26ef77c211423472bf94bbf0cc6e3ca
Matt Bishop <matt.bishop@readinghorizons.com>
2020-01-09 16:21:26 -0700

    Got black pixel coming in from texture.

--- res/basicShader.fs
+++ res/basicShader.fs
@@ -1,6 +1,9 @@
 #version 120
 
+// uniform is updated by the CPU, CPU write, GPU read
+uniform sampler2D diffuse;
+
 void main()
 {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = texture2D(diffuse, vec2(0.2, 0.2));// vec4(1.0, 0.0, 0.0, 1.0);
 }
